c63cccd0c4b3f6947e8148031288a5d0a6032f53
Bshields87 <shields.bethany@gmail.com>
2019-03-19 17:43:11 -0700

    ?

---
+++ .env.js
@@ -0,0 +1,6 @@
+
+
+function Spotify(){
+let SPOTIFY_ID = "015d9c94eeba42a6a6bdac8325c705f6"
+let SPOTIFY_SECRET = "9d834df4038c4f798c530291f9c76d05"
+}
--- liri.js
+++ liri.js
@@ -1,3 +1,20 @@
-require("dotenv").config();
+ require("dotenv").config();
 
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
+
+
+var spotify = new Spotify({
+    id: "015d9c94eeba42a6a6bdac8325c705f6",
+    secret: "9d834df4038c4f798c530291f9c76d05"
+  });
+var query= process.argv[2];
+spotify.search({ type: 'track', query: query }, function(err, data) {
+ 
+    
+    if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+  console.log(data.artist);
+});
