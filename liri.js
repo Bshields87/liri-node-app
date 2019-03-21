aec9f314d4e68f13e68379ae6c800477c563f9d0
Bshields87 <shields.bethany@gmail.com>
2019-03-20 19:32:25 -0700

    concert this and movie this working

--- liri.js
+++ liri.js
@@ -1,24 +1,57 @@
  require("dotenv").config();
-
-
+let keys = require('./keys.js');
+//var spotify = new Spotify(keys.spotify);
+let fs= require("fs");
+let moment = require("moment");
 var axios = require("axios");
 
-/*var action= process.argv[2];
-var artist= process.argv[3];
+
+var divider = "\n------------------------------------------------------------\n\n";
+
+var action= process.argv[2];
+
+if (action === "concert-this"){
+var artist= process.argv.slice(3).join("");
 axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
   function(response){
-    console.log(response.data);
-  });
-
-
-  var movieTitle= process.argv[3];
-  axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log(response);
+    let jsonData = response.data;
+ 
+    for (i = 0; i < jsonData.length; i++)  {
+    let concertSchedule = [
+      "Artist: " + jsonData[i].lineup[0],
+      "Venue: " + jsonData[i].venue.name,
+     "Location: " + jsonData[i].venue.country + jsonData[i].venue.city,
+     "Date: " + moment(jsonData[i].datetime).format("MM/DD/YYYY, h:mm:ss a"),
+     divider
+    ]
+    console.log(concertSchedule);
   }
-  );*/
+  });
+}
 
-var fs= require("fs");
+else if (action === "movie-this"){
+  var movieTitle= process.argv.slice(3).join("");
+  axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&apikey=trilogy").then(
+  function(response) {
+
+    let jsonData = response.data;
+  
+     let movieInfo = [
+        "Title: " + jsonData.Title,
+        "Release Year: " + jsonData.Year,
+       "IMDB Rating: " + jsonData.imdbRating,
+       "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
+        "Country of Production: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot,
+        "Actors: " + jsonData.Actors    ]
+    console.log(movieInfo);
+
+   
+  });
+}
+
+
 fs.readFile("random.txt","utf8", function(error, data){
   if (error){
     return console.log(error);
